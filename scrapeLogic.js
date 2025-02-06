b690a8805789bdebaf293c942a460259e8b85a5e
khanhcx5 <khanhcx5@gmail.com>
2025-02-06 16:50:45 +0700

    Updated args and executable

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,19 @@
 const puppeteer = require("puppeteer");
-
+require("dotenv").config();
 const scrapeLogic = async (res) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
+        executablePath:
+            process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
+    });
+    try {
     const page = await browser.newPage();
 
     // Navigate the page to a URL
@@ -28,8 +40,13 @@ const scrapeLogic = async (res) => {
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
     res.send(logStatement);
-    
+    } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+    } finally {
     await browser.close();
+    }
+    
 };
 
 module.exports = { scrapeLogic };
